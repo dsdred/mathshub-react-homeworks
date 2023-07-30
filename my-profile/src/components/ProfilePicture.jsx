1c601782cd966deb5e70a52b4141f53c01a00954
dsdred <dred_ne@mail.ru>
2023-07-31 00:16:59 +0500

    feat: add componet ProfilePictore

---
+++ my-profile/src/components/ProfilePicture.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProfilePicture() {
+  return (
+    <img
+      src="https://avatars.githubusercontent.com/u/67696364?v=4"
+      alt="avatar"
+    />
+  );
+}
+
+export default ProfilePicture;
--- my-profile/src/index.jsx
+++ my-profile/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import ProfilePicture from "./components/ProfilePicture";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<ProfilePicture />);
