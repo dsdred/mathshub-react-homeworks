ec1327fcf14173800a2ecdefd630def06ee44ee7
dsdred <dred_ne@mail.ru>
2023-07-31 00:38:17 +0500

    feat: add componet ProfileCitation

--- my-profile/src/components/Profile.jsx
+++ my-profile/src/components/Profile.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ProfilePicture from "./ProfilePicture";
 import ProfileName from "./ProfileName";
+import ProfileBio from "./ProfileBio";
+import ProfileCitation from "./ProfileCitation";
 
 function Profile() {
   return (
     <>
       <ProfilePicture />
       <ProfileName />
+      <ProfileBio />
+      <ProfileCitation />
     </>
   );
 }
---
+++ my-profile/src/components/ProfileCitation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfileCitation() {
+  return (
+    <blockquote>
+      День, когда вы перестаете учиться, должен быть днем вашей смерти.
+    </blockquote>
+  );
+}
+
+export default ProfileCitation;
