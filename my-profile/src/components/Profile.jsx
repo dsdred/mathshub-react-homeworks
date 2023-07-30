2955041bc72dddc66cb4693b6d7088803f83ce62
dsdred <dred_ne@mail.ru>
2023-07-31 00:27:33 +0500

    feat: add componet Profile and ProfileName

---
+++ my-profile/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfilePicture from "./ProfilePicture";
+import ProfileName from "./ProfileName";
+
+function Profile() {
+  return (
+    <>
+      <ProfilePicture />
+      <ProfileName />
+    </>
+  );
+}
+
+export default Profile;
