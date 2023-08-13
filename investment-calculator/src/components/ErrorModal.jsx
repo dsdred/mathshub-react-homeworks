5d978678293f40dc75f6bec4919d4cabfc505ab1
dsdred <dred_ne@mail.ru>
2023-08-14 01:43:30 +0500

    feat: add Portals

--- investment-calculator/src/components/ErrorModal.jsx
+++ investment-calculator/src/components/ErrorModal.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./ErrorModal.css";
 
 function ErrorModal({ title, message, onClose }) {
   return (
     <>
-      <div className="overlay" />
-      <div className="modal">
-        <header className="headerColor">
-          <h2>{title}</h2>
-          <div className="content">
-            <p>{message}</p>
-          </div>
-          <button className="button" onClick={onClose}>
-            OK
-          </button>
-        </header>
-      </div>
+      {ReactDOM.createPortal(
+        <div className="overlay" onClick={onClose} />,
+        document.getElementById("overlay-root")
+      )}
+      {ReactDOM.createPortal(
+        <div className="modal">
+          <header className="headerColor">
+            <h2>{title}</h2>
+            <div className="content">
+              <p>{message}</p>
+            </div>
+            <button className="button" onClick={onClose}>
+              OK
+            </button>
+          </header>
+        </div>,
+        document.getElementById("modal-root")
+      )}
     </>
   );
 }
