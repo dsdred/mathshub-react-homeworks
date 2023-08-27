c3641e20c659d7aaa4e16837c610fdc4dace43ca
dsdred <dred_ne@mail.ru>
2023-08-27 21:37:56 +0500

    feat: add Body-LeftMenu

--- old-vk-wall/src/App.jsx
+++ old-vk-wall/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 
 import MainHeader from "./components/Head/MainHeader";
+import MainBody from "./components/Body/MainBody";
 import MainFooter from "./components/Foot/MainFooter";
 
 function App() {
   return (
     <>
       <MainHeader />
-      <div>body</div>
+      <MainBody />
       <MainFooter />
     </>
   );
---
+++ old-vk-wall/src/components/Body/MainBody.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./MainBody.css";
+import LeftForm from "./LeftForm";
+
+function MainBody() {
+  return (
+    <div className="bodyStyle">
+      <LeftForm />
+      <div>body</div>
+    </div>
+  );
+}
+
+export default MainBody;
